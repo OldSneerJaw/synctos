a61cbc24388a1438616cb354f16349cdfa262083
Joel Andrews <joel.andrews@kashoo.com>
2016-06-08 11:26:48 -0700

    Fix the filename of the make-sync-function script in the test runner

--- samples/run-tests.sh
+++ samples/run-tests.sh
@@ -3,6 +3,6 @@
 # Create a temporary sync function for sample-sync-doc-definitions.js and then runs all of its unit tests with Mocha.
 mkdir -p target/
 
-../sync-function-maker.rb sample-sync-doc-definitions.js target/test-sample-sync-function.js
+../make-sync-function.rb sample-sync-doc-definitions.js target/test-sample-sync-function.js
 
 node_modules/.bin/mocha
