837cfdd448a602416c81a45bae43f11ba0ae3770
Joel Andrews <oldsneerjaw@gmail.com>
2017-01-23 19:00:12 -0800

    Issue #75: Extract user access assignment into a new module
    Functions and variables that are specifically used in assigning access to users and roles are now loaded into the sync function template from a module defined in a separate file.

--- etc/sync-function-template.js
+++ etc/sync-function-template.js
@@ -45,111 +45,8 @@ function synctos(doc, oldDoc) {
   // Load the document validation module
   var validationModule = includeSyncFunctionFragment('sync-function-validation-module.js')();
 
-  // Adds a prefix to the specified item if the prefix is defined
-  function prefixItem(item, prefix) {
-    return (prefix ? prefix + item : item.toString());
-  }
-
-  // Transforms the given item or items into a new list of items with the specified prefix (if any) appended to each element
-  function resolveCollectionItems(originalItems, itemPrefix) {
-    if (isValueNullOrUndefined(originalItems)) {
-      return [ ];
-    } else if (originalItems instanceof Array) {
-      var resultItems = [ ];
-      for (var i = 0; i < originalItems.length; i++) {
-        var item = originalItems[i];
-
-        if (isValueNullOrUndefined(item)) {
-          continue;
-        }
-
-        resultItems.push(prefixItem(item, itemPrefix));
-      }
-
-      return resultItems;
-    } else {
-      // Represents a single item
-      return [ prefixItem(originalItems, itemPrefix) ];
-    }
-  }
-
-  // Transforms the given collection definition, which may have been defined as a single item, a list of items or a function that returns a
-  // list of items into a simple list, where each item has the specified prefix, if any
-  function resolveCollectionDefinition(doc, oldDoc, collectionDefinition, itemPrefix) {
-    if (isValueNullOrUndefined(collectionDefinition)) {
-      return [ ];
-    } else {
-      if (typeof(collectionDefinition) === 'function') {
-        var fnResults = collectionDefinition(doc, oldDoc);
-
-        return resolveCollectionItems(fnResults, itemPrefix);
-      } else {
-        return resolveCollectionItems(collectionDefinition, itemPrefix);
-      }
-    }
-  }
-
-  // Transforms a role collection definition into a simple list and prefixes each element with "role:"
-  function resolveRoleCollectionDefinition(doc, oldDoc, rolesDefinition) {
-    return resolveCollectionDefinition(doc, oldDoc, rolesDefinition, 'role:');
-  }
-
-  // Assigns channel access to users/roles
-  function assignChannelsToUsersAndRoles(doc, oldDoc, accessAssignmentDefinition) {
-    var usersAndRoles = [ ];
-
-    var users = resolveCollectionDefinition(doc, oldDoc, accessAssignmentDefinition.users);
-    for (var userIndex = 0; userIndex < users.length; userIndex++) {
-      usersAndRoles.push(users[userIndex]);
-    }
-
-    var roles = resolveRoleCollectionDefinition(doc, oldDoc, accessAssignmentDefinition.roles);
-    for (var roleIndex = 0; roleIndex < roles.length; roleIndex++) {
-      usersAndRoles.push(roles[roleIndex]);
-    }
-
-    var channels = resolveCollectionDefinition(doc, oldDoc, accessAssignmentDefinition.channels);
-
-    access(usersAndRoles, channels);
-
-    return {
-      type: 'channel',
-      usersAndRoles: usersAndRoles,
-      channels: channels
-    };
-  }
-
-  // Assigns role access to users
-  function assignRolesToUsers(doc, oldDoc, accessAssignmentDefinition) {
-    var users = resolveCollectionDefinition(doc, oldDoc, accessAssignmentDefinition.users);
-    var roles = resolveRoleCollectionDefinition(doc, oldDoc, accessAssignmentDefinition.roles);
-
-    role(users, roles);
-
-    return {
-      type: 'role',
-      users: users,
-      roles: roles
-    };
-  }
-
-  // Assigns role access to users and/or channel access to users/roles according to the given access assignment definitions
-  function assignUserAccess(doc, oldDoc, accessAssignmentDefinitions) {
-    var effectiveOldDoc = getEffectiveOldDoc(oldDoc);
-
-    var effectiveAssignments = [ ];
-    for (var assignmentIndex = 0; assignmentIndex < accessAssignmentDefinitions.length; assignmentIndex++) {
-      var definition = accessAssignmentDefinitions[assignmentIndex];
-
-      if (definition.type === 'role') {
-        effectiveAssignments.push(assignRolesToUsers(doc, effectiveOldDoc, definition));
-      } else if (definition.type === 'channel' || isValueNullOrUndefined(definition.type)) {
-        effectiveAssignments.push(assignChannelsToUsersAndRoles(doc, effectiveOldDoc, definition));
-      }
-    }
-
-    return effectiveAssignments;
-  }
+  // Load the access assignment module
+  var accessAssignmentModule = includeSyncFunctionFragment('sync-function-access-assignment-module.js')();
 
   var rawDocDefinitions = %SYNC_DOCUMENT_DEFINITIONS%;
 
@@ -211,7 +108,7 @@ function synctos(doc, oldDoc) {
   }
 
   if (theDocDefinition.accessAssignments && theDocDefinition.accessAssignments.length > 0) {
-    customActionMetadata.accessAssignments = assignUserAccess(doc, oldDoc, theDocDefinition.accessAssignments);
+    customActionMetadata.accessAssignments = accessAssignmentModule.assignUserAccess(doc, oldDoc, theDocDefinition.accessAssignments);
 
     if (theDocDefinition.customActions && typeof(theDocDefinition.customActions.onAccessAssignmentsSucceeded) === 'function') {
       theDocDefinition.customActions.onAccessAssignmentsSucceeded(doc, oldDoc, customActionMetadata);
