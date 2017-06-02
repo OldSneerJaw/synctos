60534d9cea298bd8453e54d7ce5460b60becc2c0
Joel <oldsneerjaw@gmail.com>
2017-06-02 16:18:54 -0700

    Issue #43: Validation of channels, roles and users

--- etc/document-definitions-validator.js
+++ etc/document-definitions-validator.js
@@ -13,7 +13,7 @@ function validate(rawDocDefinitions) {
     var docDefinition = docDefinitions[docType];
 
     if (!isAnObject(docDefinition)) {
-      validationErrors.push('document type "' + docType + '" is not defined as an object');
+      validationErrors.push(docTypePrefix(docType) + ' is not defined as an object');
     } else {
       validateDocType(docType, docDefinition);
     }
@@ -28,94 +28,121 @@ function validate(rawDocDefinitions) {
 
       switch(propertyName) {
         case 'typeFilter':
-          if (typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "typeFilter" property is not a function');
-          }
           hasTypeFilter = true;
+          if (typeof(propertyValue) !== 'function') {
+            validationErrors.push(docTypePrefix(docType) + ': the "typeFilter" property is not a function');
+          }
           break;
         case 'channels':
-          if (!isAnObject(propertyValue) && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "channels" property is not an object');
-          }
           hasPermissionGrant = true;
+          if (isAnObject(propertyValue)) {
+            validatePermissions('channels', propertyValue);
+          } else if (typeof(propertyValue) !== 'function') {
+            validationErrors.push(docTypePrefix(docType) + ': the "channels" property is not an object or a function');
+          }
           break;
         case 'authorizedRoles':
-          if (!isAnObject(propertyValue) && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "authorizedRoles" property is not an object');
-          }
           hasPermissionGrant = true;
+          if (isAnObject(propertyValue)) {
+            validatePermissions('authorizedRoles', propertyValue);
+          } else if (typeof(propertyValue) !== 'function') {
+            validationErrors.push(docTypePrefix(docType) + ': the "authorizedRoles" property is not an object or a function');
+          }
           break;
         case 'authorizedUsers':
-          if (!isAnObject(propertyValue) && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "authorizedUsers" property is not an object');
-          }
           hasPermissionGrant = true;
+          if (isAnObject(propertyValue)) {
+            validatePermissions('authorizedUsers', propertyValue);
+          } else if (typeof(propertyValue) !== 'function') {
+            validationErrors.push(docTypePrefix(docType) + ': the "authorizedUsers" property is not an object or a function');
+          }
           break;
         case 'propertyValidators':
-          if (!isAnObject(propertyValue) && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "propertyValidators" property is not an object');
-          }
           hasPropertyValidators = true;
+          if (!isAnObject(propertyValue) && typeof(propertyValue) !== 'function') {
+            validationErrors.push(docTypePrefix(docType) + ': the "propertyValidators" property is not an object or a function');
+          }
           break;
         case 'allowUnknownProperties':
           if (typeof(propertyValue) !== 'boolean' && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "allowUnknownProperties" property is not a boolean');
+            validationErrors.push(docTypePrefix(docType) + ': the "allowUnknownProperties" property is not a boolean or a function');
           }
           break;
         case 'immutable':
           if (typeof(propertyValue) !== 'boolean' && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "immutable" property is not a boolean');
+            validationErrors.push(docTypePrefix(docType) + ': the "immutable" property is not a boolean or a function');
           }
           break;
         case 'cannotReplace':
           if (typeof(propertyValue) !== 'boolean' && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "cannotReplace" property is not a boolean');
+            validationErrors.push(docTypePrefix(docType) + ': the "cannotReplace" property is not a boolean or a function');
           }
           break;
         case 'cannotDelete':
           if (typeof(propertyValue) !== 'boolean' && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "cannotDelete" property is not a boolean');
+            validationErrors.push(docTypePrefix(docType) + ': the "cannotDelete" property is not a boolean or a function');
           }
           break;
         case 'allowAttachments':
           if (typeof(propertyValue) !== 'boolean' && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "allowAttachments" property is not a boolean');
+            validationErrors.push(docTypePrefix(docType) + ': the "allowAttachments" property is not a boolean or a function');
           }
           break;
         case 'attachmentConstraints':
           if (!isAnObject(propertyValue) && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "attachmentConstraints" property is not an object');
+            validationErrors.push(docTypePrefix(docType) + ': the "attachmentConstraints" property is not an object or a function');
           }
           break;
         case 'accessAssignments':
           if (!(propertyValue instanceof Array) && typeof(propertyValue) !== 'string' && typeof(propertyValue) !== 'function') {
-            validationErrors.push('document type "' + docType + '": the "accessAssignments" property is not an array or string value');
+            validationErrors.push(docTypePrefix(docType) + ': the "accessAssignments" property is not an array or a string or a function');
           }
           break;
         case 'customActions':
           if (!isAnObject(propertyValue)) {
-            validationErrors.push('document type "' + docType + '": the "customActions" property is not an object');
+            validationErrors.push(docTypePrefix(docType) + ': the "customActions" property is not an object');
           }
           break;
         default:
-          validationErrors.push('document type "' + docType + '" includes unsupported property: "' + propertyName + '"');
+          validationErrors.push(docTypePrefix(docType) + ' includes unsupported property: "' + propertyName + '"');
           break;
       }
     }
 
     if (!hasTypeFilter) {
-      validationErrors.push('document type "' + docType + '" is missing a typeFilter property');
+      validationErrors.push(docTypePrefix(docType) + ' is missing a typeFilter property');
     }
     if (!hasPermissionGrant) {
-      validationErrors.push('document type "' + docType + '" is missing a channels, authorizedRoles or authorizedUsers property');
+      validationErrors.push(docTypePrefix(docType) + ' is missing a channels, authorizedRoles or authorizedUsers property');
     }
     if (!hasPropertyValidators) {
-      validationErrors.push('document type "' + docType + '" is missing a propertyValidators property');
+      validationErrors.push(docTypePrefix(docType) + ' is missing a propertyValidators property');
+    }
+  }
+
+  function validatePermissions(permissionsCategory, permissionsDefinition) {
+    for (var permissionOperation in permissionsDefinition) {
+      var permissions = permissionsDefinition[permissionOperation];
+
+      if (!supportedPermissionOperations.has(permissionOperation)) {
+        validationErrors.push(docTypePrefix(docType) + ': the "' + permissionsCategory + '" property\'s "' + permissionOperation + '" operation type is not supported');
+      } else if (permissions instanceof Array) {
+        for (var permissionIndex = 0; permissionIndex < permissions.length; permissionIndex++) {
+          var permission = permissions[permissionIndex];
+          if (typeof(permission) !== 'string') {
+            validationErrors.push(docTypePrefix(docType) + ': the "' + permissionsCategory + '" property\'s "' + permissionOperation + '" operation contains an entry that is not a string: ' + JSON.stringify(permission));
+          }
+        }
+      } else if (typeof(permissions) !== 'string') {
+        validationErrors.push(docTypePrefix(docType) + ': the "' + permissionsCategory + '" property\'s "' + permissionOperation + '" operation is not a string or array');
+      }
     }
   }
 
   return validationErrors;
 }
+
+var supportedPermissionOperations = new Set([ 'view', 'add', 'replace', 'remove', 'write' ]);
 
 function resolveDocDefinitions(rawDocDefinitions) {
   return (typeof(rawDocDefinitions) === 'function') ? rawDocDefinitions() : rawDocDefinitions;
@@ -123,4 +150,8 @@ function resolveDocDefinitions(rawDocDefinitions) {
 
 function isAnObject(value) {
   return typeof(value) === 'object' && !(value instanceof Array);
+}
+
+function docTypePrefix(docType) {
+  return 'document type "' + docType + '"';
 }
