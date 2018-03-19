ee6a173eb5f285effb00aa8eaa23911b0909aa6d
Joel <oldsneerjaw@gmail.com>
2018-03-19 16:39:24 -0700

    Remove debug console.log statement from test case

--- test/datetime.spec.js
+++ test/datetime.spec.js
@@ -401,8 +401,6 @@ describe('Date/time validation type', () => {
         exclusiveRangeValidationAsDatetimesProp: new Date('2018-02-08T12:22:37.900-0500').toISOString() // Output as UTC
       };
 
-      console.log('exclusiveRangeValidationAsDatetimesProp: ' + doc.exclusiveRangeValidationAsDatetimesProp);
-
       testFixture.verifyDocumentNotCreated(
         doc,
         'datetimeDoc',
